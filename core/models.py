2db69198367167d6e775e7c1e46b473110f1e469
NickUkolov <ukolov_1996@list.ru>
2022-12-16 18:06:21 +0400

    templates, settings, feed, signing in and up added

--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
+from datetime import datetime
+import uuid
+from django.utils import timezone
 from django.contrib.auth import get_user_model
-
 from django.db import models
-
 
 User = get_user_model()
 
+
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-photo.jpeg')
@@ -14,3 +16,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    likes_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
